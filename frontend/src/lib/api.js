8903ab5ea72d5fc3e8da66ba35293b8bea4a3a8e
disharaghav <disharaghav0@gmail.com>
2026-03-18 20:18:08 +0530

    Update api.js

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.REACT_APP_API_URL; // ✅ FIXED
+const API_BASE = `${process.env.REACT_APP_API_URL}/api`; // ✅ FIXED
 
 const api = axios.create({
     baseURL: API_BASE,
